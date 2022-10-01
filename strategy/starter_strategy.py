461ec84288a8dd18a26fa4b9a26198836b8c1181
Jacob Stolker <stolkerj@yahoo.com>
2022-09-30 21:31:05 -0500

    made a basic strategy

--- strategy/starter_strategy.py
+++ strategy/starter_strategy.py
@@ -6,16 +6,34 @@ from game.item import Item
 
 from game.position import Position
 from strategy.strategy import Strategy
+from util.utility import chebyshev_distance
 
 class StarterStrategy(Strategy):
     def strategy_initialize(self, my_player_index: int):
-        return game.character_class.CharacterClass.WIZARD
+        return game.character_class.CharacterClass.ARCHER
 
     def move_action_decision(self, game_state: GameState, my_player_index: int) -> Position:
-        return game_state.player_state_list[my_player_index].position
+        position = game_state.player_state_list[my_player_index].position
+        next_pos = Position(position.x,position.y)
+        if position.x == 0:
+            next_pos.x = 4
+        elif position.x == 9:
+            next_pos.x = 5
+        if position.y == 0:
+            next_pos.y = 4
+        elif position.y == 9:
+            next_pos.y = 5
+
+
+        return next_pos
 
     def attack_action_decision(self, game_state: GameState, my_player_index: int) -> int:
-        return Random().randint(0, 3)
+        for i in range(4):
+            if i != my_player_index:
+                dist = chebyshev_distance(game_state.player_state_list[my_player_index].position, game_state.player_state_list[i].position)
+                if dist <= 3:
+                    return i
+        return my_player_index
 
     def buy_action_decision(self, game_state: GameState, my_player_index: int) -> Item:
         return Item.NONE
--- util/utility.py
+++ util/utility.py
@@ -7,14 +7,14 @@ import config
 from game.position import Position
 
 def manhattan_distance(p1: Position, p2: Position) -> int:
-    return math.abs(p1.getX() - p2.getX()) + math.abs(p1.getY() - p2.getY())
+    return abs(p1.x - p2.x) + abs(p1.y - p2.y)
 
 def chebyshev_distance(p1: Position, p2: Position) -> int:
-    return math.max(math.abs(p1.getX() - p2.getX()), math.abs(p1.getY() - p2.getY()))
+    return max(abs(p1.x - p2.x), abs(p1.y - p2.y))
 
 def in_bounds(p: Position) -> bool:
     #  Assume board runs from 0 to BOARD_SIZE - 1
-    return ((p.getX() >= 0) and (p.getX() < config.BOARD_SIZE) and (p.getY() >= 0) and (p.getY() < config.BOARD_SIZE))
+    return ((p.x >= 0) and (p.x < config.BOARD_SIZE) and (p.y >= 0) and (p.y < config.BOARD_SIZE))
 
 def random_enum(clazz: Enum):
     return random.choice(list(clazz))
